fa350a7c0ad0dff6c5d6135ed1260042a9ed27d5
shroukshamselden <shrouk.shamselden@gmail.com>
2023-06-15 19:16:56 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int ch = 1;
+int in = 4;
+int long_i = 4;
+int long_li = 8;
+int flo = 4;
+
+printf("Size of a char: %d byte(s)\n", ch);
+printf("Size of a int: %d byte(s)\n", in);
+printf("Size of a long int: %d byte(s)\n", long_i);
+printf("Size of a long long int: %d byte(s)\n", long_li);
+printf("Size of a float: %d byte(s)\n", flo);
+	return (0);
+}
