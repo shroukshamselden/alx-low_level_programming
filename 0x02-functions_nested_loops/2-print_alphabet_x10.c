5e7cde8d1a8ae2379de11a893d0df6d4e48c2490
shroukshamselden <shrouk.shamselden@gmail.com>
2023-06-20 19:28:44 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets
+ * Describtion: prints lowwer alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while
+		(i < 10);
+	{
+		ch = 'a';
+		while
+			(ch <= 'z');
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+			i++;
+	}
+}
