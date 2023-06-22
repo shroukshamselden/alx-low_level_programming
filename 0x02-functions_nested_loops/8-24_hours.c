a395872259aa76b0bbe2130321daf7592dc3b9a7
shroukshamselden <shrouk.shamselden@gmail.com>
2023-06-21 19:56:22 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+
+for (h = 0; h < 24; h++)
+for (m = 0; m < 60; m++)
+
+_putchar('0' + (h / 10));
+_putchar('0' + (h % 10));
+_putchar(':');
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
+_putchar('\n');
+}
