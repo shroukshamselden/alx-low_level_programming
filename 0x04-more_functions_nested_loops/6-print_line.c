317b0c0853efdf6339028f1c6de39b2b7ec49e38
shroukshamselden <shrouk.shamselden@gmail.com>
2023-06-22 18:46:49 -0700

    66

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,20 @@
 
 void print_line(int n)
 {
-	int i;
-while (i < n)
-{
-	_putchar('_');
-	i++;
-}
+	int i = 0;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
 _putchar('\n');
 
 }
