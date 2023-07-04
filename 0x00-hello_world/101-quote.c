bbacbbc912c63ac489100cc26d89010e9e03bd9c
shroukshamselden <shrouk.shamselden@gmail.com>
2023-07-04 15:33:01 -0700

    ad

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point
- *
- *Return: Always 0 (Success)
+ * Return: 0
  */
+
 int main(void)
 {
-const char message[] = "It's turtles all the way down\n";
-write(STDOUT_FILENO, message, sizeof(message) -1);
-       	return (1);
+	char *msg = "and that piece of art is useful\n";
+
+	write(2, msg, 31);
+	return (1);
 
 }
