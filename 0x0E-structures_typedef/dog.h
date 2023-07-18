d546993034ef673159a68dee0568135072ecf413
shroukshamselden <shrouk.shamselden@gmail.com>
2023-07-17 17:46:32 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+ /**
+ * struct dog - a dog's info
+ * @name: First
+ * @age: Second
+ * @owner: Third
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
