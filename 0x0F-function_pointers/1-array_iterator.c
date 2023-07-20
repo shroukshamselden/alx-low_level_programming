e5cd0d87a09a357bbd79f374137660eb0279e81c
shroukshamselden <shrouk.shamselden@gmail.com>
2023-07-19 17:12:11 -0700

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-for (i = 0; i < size; i++)
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
 	action(array[i]);
 }
